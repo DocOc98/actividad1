c44f7f13b121aaeb5c99f47a467d534711eddf8f
Octavio de la Vega <h.o.delavega@gmail.com>
2021-04-28 02:12:34 -0300

    Creacion de la TodoList

---
+++ src/components/Form.jsx
@@ -0,0 +1,24 @@
+const Form = ({
+    handleSubmit,
+    updateState,
+    puesto,
+    empresa,
+    ciudad,
+    pais,
+  }) => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="puesto" className="form-control" placeholder="Nombre del Puesto" onChange={updateState} value={puesto} />
+        <br />
+        <input type="text" name="empresa" className="form-control" placeholder="Ingresa empresa" onChange={updateState} value={empresa} />
+        <br />
+        <input type="text" name="ciudad" className="form-control" placeholder="Ingresa ciudad" onChange={updateState} value={ciudad} />
+        <br />
+        <input type="text" name="pais" className="form-control" placeholder="Ingresa pais" onChange={updateState} value={pais} />
+        <br />
+        <button type="submit" className="btn btn-primary btn-success">Agregar</button>
+      </form>
+    );
+  };
+  
+  export default Form;
