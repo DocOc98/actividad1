297eed266fff7c6f35fcea4db3cee440ec37e269
Octavio de la Vega <h.o.delavega@gmail.com>
2021-05-06 19:26:52 -0300

    Modificación de List

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,11 +2,14 @@ const List = ({ td, handleDelete }) => {
     return (
       <ul class="list-group" key={td.id}>
         <li className="list-group-item mb-3">
+          <h4>
+            {td.puesto}
+          </h4>
+          <h6>
+            {td.empresa}
+          </h6>
           <p>
-            <b>Puesto</b>: {td.puesto} - <b>Empresa</b>: {td.empresa}
-          </p>
-          <p>
-            <b>Ciudad</b>: {td.ciudad} - <b>Pais</b>: {td.pais}
+            {td.ciudad}, {td.pais}
           </p>
           <button onClick={() => handleDelete(td.id)} className="btn btn-danger float-right">Eliminar</button>
         </li>
